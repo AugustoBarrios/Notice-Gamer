2150be4a0ab756d5434e9fd51d53fb5e118cc8b7
Augusto Barrios <augustobg02@gmail.com>
2022-08-22 16:21:46 +0200

    Componente NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Componentes/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Componentes/NavBar.js
@@ -0,0 +1,29 @@
+import "./NavBar.css";
+
+
+function Menu() {
+    return (
+        <div className="ContenedorDeMenu">
+            <div className="Buscador">
+                <p className="NombrePagina">NTG / Noticia Gamer</p>
+                <input className="InputBuscador" type="text" placeholder="Buscar"></input>
+                <button className="BotonBuscador">Buscar</button></div>
+            <div className="ContenedorDeLista">
+                <nav>
+                    <ul className="Lista Navegacion">
+                        <li className="opciones"><a className="LinkMenu" href="#">Inicio</a></li>
+                        <li className="opciones"><a className="LinkMenu" href="#">Noticias</a></li>
+                        <li className="opciones"><a className="LinkMenu" href="#">Acerca de</a></li>
+                        <li className="opciones"><a className="LinkMenu" href="#">Contacto</a></li>
+                    </ul>
+                    <ul className="Lista Interaccion">
+                        <li className="opciones"><a className="LinkMenu" href="#">Registrarse</a></li>
+                        <li className="opciones" ><a className="LinkMenu" href="#">Acceder a tu cuenta</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
